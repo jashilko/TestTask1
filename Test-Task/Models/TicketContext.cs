70ace192c3b4b39f0b05319e5bebb7e909fe46d6
Konstantin Shiliaev <jashilko@gmail.com>
2016-10-14 19:56:36 +0300

    [*] Добавляем исходные коды первой версии

---
+++ Test-Task/Models/TicketContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Test_Task.Models
+{
+    public class TicketContext: DbContext
+    {
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
